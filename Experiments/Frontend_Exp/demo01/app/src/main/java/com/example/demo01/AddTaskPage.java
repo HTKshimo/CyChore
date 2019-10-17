61e39d5fb85782417516bfc6e850c5c8d8bf0cb2
Yamini Mathur <yamini@iastate.edu>
2019-10-17 18:23:22 -0500

    Updates - Forgot what I changed

--- Experiments/Frontend_Exp/demo01/app/src/main/java/com/example/demo01/AddTaskPage.java
+++ Experiments/Frontend_Exp/demo01/app/src/main/java/com/example/demo01/AddTaskPage.java
@@ -1,6 +1,40 @@
 package com.example.demo01;
 
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import androidx.appcompat.app.AppCompatActivity;
+
 class AddTaskPage
 {
+    public class GroupDefaultPage extends AppCompatActivity
+    {
+        Button createTask;
 
+        public void onCreate(Bundle savedInstanceState)
+        {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+
+            createTask=(Button)findViewById(R.id.add_task);
+            createTask.setOnClickListener(new View.OnClickListener()
+            {
+                @Override
+                public void onClick(View v) {
+                    // TODO Auto-generated method stub
+                    //Intent i = new Intent(getApplicationContext(),SendPhotos.class);
+                    // startActivity(i);
+                    if(v.getId() == R.id.add_task)
+                    {
+                        Intent i = new Intent(com.example.demo01.AddTaskPage.this, AddTaskPage.class);
+                        startActivity(i);
+                    }
+                }
+            });
+
+        }
+
+    }
 }
