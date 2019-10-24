d06ff537861f1e0eb3294892a672909ef1520f8f
Yamini Mathur <yamini@iastate.edu>
2019-10-24 09:14:34 -0500

    Commented Mock and Test

--- Experiments/Frontend_Exp/demo01/app/src/main/java/ExampleUnitTest.java
+++ Experiments/Frontend_Exp/demo01/app/src/main/java/ExampleUnitTest.java
@@ -31,9 +31,9 @@ public class ExampleUnitTest
 {
     private static final String TEST_STRING = "HELLO WORLD!";
     //As we don't have access to Context in our JUnit test classes, we need to mock it
-   /// @Mock
+
     Context mMockContext;
-  //  @Test
+
    // public void readStringFromContext() {
         //Returns the TEST_STRING when getString(R.string.hello_world) is called
         //when(mMockContext.getString(R.string.text_hello_word)).thenReturn(TEST_STRING);
