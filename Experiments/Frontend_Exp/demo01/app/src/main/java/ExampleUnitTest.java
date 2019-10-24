9c1df98f905d16221dd0ea3764260898e18c5754
Yamini Mathur <yamini@iastate.edu>
2019-10-24 09:10:48 -0500

    Add user task

--- Experiments/Frontend_Exp/demo01/app/src/main/java/ExampleUnitTest.java
+++ Experiments/Frontend_Exp/demo01/app/src/main/java/ExampleUnitTest.java
@@ -3,9 +3,30 @@ import android.content.Context;
 import com.example.demo01.R;
 import com.example.demo01.ui.ClassUnderTest;
 
+/*import androidx.test.filters.LargeTest;
+import androidx.test.rule.ActivityTestRule;
+import androidx.test.runner.AndroidJUnit4;
 
-/*How to use Mockito: https://dzone.com/articles/how-to-use-mockito-in-android*/
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.runner.RunWith;
 
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.action.ViewActions.click;
+import static androidx.test.espresso.action.ViewActions.typeText;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
+import static androidx.test.espresso.matcher.ViewMatchers.isDisplayed;
+import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static androidx.test.espresso.matcher.ViewMatchers.withText;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.notNullValue;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+
+
+How to use Mockito: https://dzone.com/articles/how-to-use-mockito-in-android
+@RunWith(AndroidJUnit4.class)
+@LargeTest*/
 public class ExampleUnitTest
 {
     private static final String TEST_STRING = "HELLO WORLD!";
@@ -13,7 +34,7 @@ public class ExampleUnitTest
    /// @Mock
     Context mMockContext;
   //  @Test
-    public void readStringFromContext() {
+   // public void readStringFromContext() {
         //Returns the TEST_STRING when getString(R.string.hello_world) is called
         //when(mMockContext.getString(R.string.text_hello_word)).thenReturn(TEST_STRING);
         //Creates an object of the ClassUnderTest with the mock context
