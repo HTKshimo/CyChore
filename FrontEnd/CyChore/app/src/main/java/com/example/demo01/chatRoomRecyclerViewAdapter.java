c23ab2de23e3380074c291b78bb0f762af366bfd
mohanz <mohanz@iastate.edu>
2019-11-06 16:32:45 -0600

    Moving frontend to frontend folder

---
+++ FrontEnd/CyChore/app/src/main/java/com/example/demo01/chatRoomRecyclerViewAdapter.java
@@ -0,0 +1,51 @@
+package com.example.demo01;
+
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.recyclerview.widget.RecyclerView;
+import androidx.recyclerview.widget.RecyclerView.Adapter;
+
+import java.util.List;
+
+class chatRoomRecyclerViewAdapter  extends Adapter<chatRoomRecyclerViewAdapter.ViewHolder>{
+
+    private final List<String> mValues;
+
+    public chatRoomRecyclerViewAdapter(List<String> chatContents){
+        mValues = chatContents;
+    }
+
+    @NonNull
+    @Override
+    public ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext())
+                .inflate(R.layout.chat_line_box, parent, false);
+        return new ViewHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull ViewHolder holder, int position) {
+        holder.line.setText(mValues.get(position));
+    }
+
+    @Override
+    public int getItemCount() {
+        return mValues.size();
+    }
+
+    public class ViewHolder extends RecyclerView.ViewHolder {
+        public final View mView;
+
+        public TextView line;
+
+        public ViewHolder(@NonNull View itemView) {
+            super(itemView);
+            mView = itemView;
+            line = mView.findViewById(R.id.ChatLine);
+        }
+    }
+}
