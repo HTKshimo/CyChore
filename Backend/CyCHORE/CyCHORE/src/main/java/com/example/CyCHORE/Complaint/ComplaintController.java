c1102a46b8d164bbd4ef77d436210bf948d7807e
HTKshimo <shuangl@iastate.edu>
2019-11-29 01:07:29 -0600

    created method that takes user id and status requested and return list of complaints that have given status and are assigned to admin with given user id; added score column to user table.

--- Backend/CyCHORE/CyCHORE/src/main/java/com/example/CyCHORE/Complaint/ComplaintController.java
+++ Backend/CyCHORE/CyCHORE/src/main/java/com/example/CyCHORE/Complaint/ComplaintController.java
@@ -1,21 +1,14 @@
 package com.example.CyCHORE.Complaint;
-import org.apache.tomcat.util.http.parser.HttpParser;
 import org.json.JSONArray;
 import org.json.JSONException;
 import org.json.JSONObject;
-import org.json.JSONString;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 import java.io.IOException;
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
-import java.util.Random;
 
-import java.sql.Timestamp;
-import java.text.SimpleDateFormat;
-import java.util.*;
 import java.util.stream.Collectors;
 
 import javax.servlet.http.HttpServletRequest;
@@ -28,84 +21,43 @@ public class ComplaintController {
     @Autowired
     ComplaintRepository cr;
 
-
-    //This method returns complaints made by a particular user; FOR MY COMPLAINTS
-    //This method needs uid of the user, that's all.
-    //Task status indicates whether or not a task has been completed
-    //If Task status is '0', the task is not completed
-    //If Task status is '1', the task is completed
-    @RequestMapping(value = "/getComplaintListforUser", method = POST, produces ="application/json;charset=UTF-8")
+    /**
+     * This method takes user id and status requested and return list of complaints that have given status and are assigned to admin with given user id.
+     * @param request
+     * @return
+     * @throws JSONException
+     * @throws IOException
+     */
+    @RequestMapping(value = "/getComplaint", method = POST, produces ="application/json;charset=UTF-8")
     @ResponseBody
-    public String getComplaintListforUser(HttpServletRequest request) throws JSONException, IOException {
+    public String getComplaint(HttpServletRequest request) throws JSONException, IOException {
         String data = request.getReader().lines().collect(Collectors.joining());
         JSONObject jsonObj = new JSONObject(data);
         Integer uid = Integer.valueOf((Integer) jsonObj.get("uid"));
+        Integer complaint_status = Integer.valueOf((Integer) jsonObj.get("complaint_status"));
+        JSONObject toSend = new JSONObject();
         List<Complaint> allComplaintList;
         allComplaintList = cr.findAll();
-        List<String> complaintList = new ArrayList<String>();
-        JSONObject toSend = new JSONObject();
         JSONObject Comp = new JSONObject();
+        System.out.println(uid);
+        System.out.println(complaint_status);
         int ComplaintCount = 0;
         for (Complaint temp : allComplaintList) {
-            if (temp.filer_id == uid){
-
+            if ((temp.getFiler_id() == uid || uid == 0) && (complaint_status == 5 || temp.getStatus() == complaint_status || ((temp.getStatus() == 2 || temp.getStatus() == 3) && complaint_status == 4))) {
+                System.out.println("found a complaint\n");
                 JSONObject curComp = new JSONObject();
-                //JSONObject curTask = new JSONArray();
-                curComp.put("tid",temp.getId().toString());
-                curComp.put("description", temp.getDescription().toString());
-                curComp.put("Task status:", temp.getStatus().toString());
-               // Comp.put(temp.toString(), curComp);
-                Comp.put(String.valueOf(temp.id), curComp);
+                curComp.put("title", temp.getTitle());
+                curComp.put("status", temp.getStatus().toString());
+                Comp.put(String.valueOf(temp.getId()), curComp);
                 ComplaintCount++;
             }
         }
         toSend.put("status", "0");
-        toSend.put("Number of complaints", ComplaintCount);
-        toSend.put("List of complaints:", Comp);
+        toSend.put("num_complaints", ComplaintCount);
+        toSend.put("complaints", Comp);
+
         return toSend.toString();
     }
-
-
-    //This method returns all complaints made all users; FOR ADMIN
-    //Task status indicates whether or not a task has been completed
-    //If Task status is '0', the task is not completed
-    //If Task status is '1', the task is completed
-    @RequestMapping(value = "/getAllComplaints", method = POST, produces ="application/json;charset=UTF-8")
-    @ResponseBody
-    public String getAllComplaints(HttpServletRequest request) throws JSONException, IOException {
-//        String data = request.getReader().lines().collect(Collectors.joining());
-//        JSONObject jsonObj = new JSONObject(data);
-//        Integer uid = Integer.valueOf((Integer) jsonObj.get("uid"));
-        List<Complaint> allComplaintList;
-        allComplaintList = cr.findAll();
-        List<String> complaintList = new ArrayList<String>();
-        JSONObject toSend = new JSONObject();
-        JSONObject Comp = new JSONObject();
-        JSONArray jsonArray = new JSONArray();
-        int ComplaintCount = 0;
-        for (Complaint temp : allComplaintList) {
-            //if (temp.filer_id == uid){
-
-                JSONObject curComp = new JSONObject();
-                //JSONObject curTask = new JSONArray();
-                curComp.put("tid",temp.getId().toString());
-                curComp.put("description", temp.getDescription().toString());
-                curComp.put("Task status:", temp.getStatus().toString());
-                curComp.put("Filer id", temp.getFiler_id());
-                // Comp.put(temp.toString(), curComp);
-                //Comp.put(String.valueOf(temp.id), curComp);
-            jsonArray.put(curComp);
-                ComplaintCount++;
-            //}
-        }
-        toSend.put("status", "0");
-        toSend.put("Number of complaints", ComplaintCount);
-        //toSend.put("List of complaints:", Comp.toString());
-        toSend.put("List of complaints:", jsonArray);
-        return toSend.toString();
-    }
-
-
 
 
 
