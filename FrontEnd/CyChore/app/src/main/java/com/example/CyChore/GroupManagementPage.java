00601dd84905e2614530d1da1897591aabf34155
mohanz <mohanz@iastate.edu>
2019-12-09 22:23:08 -0600

    delete unused class

---
+++ FrontEnd/CyChore/app/src/main/java/com/example/CyChore/GroupManagementPage.java
@@ -0,0 +1,14 @@
+package com.example.CyChore;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class GroupManagementPage extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_group_management_page);
+    }
+}
